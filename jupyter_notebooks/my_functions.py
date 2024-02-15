034b22d51b021c90578590565c9c40424cf1d9fc
Issam Allymis <issamally0@gmail.com>
2024-02-15 15:30:13 +0000

    Integrated heatmap to visualise the correlation of both features and target variable, Wrote conclusions for SalePrice target variable along with a visual representation to support my readings, Cleaned data prcedieng the data Cleaning notebook

---
+++ jupyter_notebooks/my_functions.py
@@ -0,0 +1,15 @@
+from feature_engine.imputation import MeanMedianImputer, CategoricalImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+
+
+def pipeline_randomforest_reg():
+        pipeline = Pipeline([
+                ('mean_median_imputer', MeanMedianImputer(imputation_method='median')),
+                ('categorical_imputer', CategoricalImputer(imputation_method='missing')),
+                ('feat_scaling', StandardScaler()),
+                ('model', RandomForestRegressor())
+        ])
+
+        return pipeline
